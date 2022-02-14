8d3bbe5219b6f9a16e860d2d5b6c135b3b77953e
tiagovianez <tiagovianez@live.com>
2022-02-14 19:36:29 -0300

    Gerador de senhas

---
+++ GeradorDeSenhas/gerador-senhas.py
@@ -0,0 +1,9 @@
+import random, string
+
+tamanho = int(input('Digite o tamanho de senha desejada: '))
+
+chars = string.ascii_letters + string.digits + '!@#$ç%&*()-=+,.;/?'
+
+rnd = random.SystemRandom()  #os.urandom
+
+print(''.join(rnd.choice(chars) for i in range(tamanho)))
